e0886c15f3f40826bec45f44a9f1a419f2c4d262
natthawannz <Natthawan.p@kkumail.com>
2026-01-07 09:44:13 +0700

    .

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
 @echo off
 REM เปิด frontend (Next.js) ในหน้าต่างแรก
-start cmd /k "cd /d C:\dev\Balanz.ia && npm run dev"
+start cmd /k "cd /d C:\dev\Balanz.zz && npm run dev"
 
 REM เปิด backend (Node.js) ในหน้าต่างที่สอง
-start cmd /k "cd /d C:\dev\Balanz.ia\backend && node index.js"
+start cmd /k "cd /d C:\dev\Balanz.zz\backend && node index.js"
 
 exit
